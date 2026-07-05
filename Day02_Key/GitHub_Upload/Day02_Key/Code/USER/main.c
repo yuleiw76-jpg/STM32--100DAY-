b9f19481c2553021d4d09e5ed25cefe9480b89f4
yuleiw76-jpg <yuleiw76@gmail.com>
2026-07-05 19:28:45 +0800

    Update Day02 key experiment

---
+++ Day02_Key/GitHub_Upload/Day02_Key/Code/USER/main.c
@@ -0,0 +1,29 @@
+#include "led.h"
+#include "delay.h"
+#include "sys.h"
+#include "key.h"
+
+
+
+int main(void)
+{	
+	u8 a;
+	delay_init();	      
+	LED_Init();
+	KEY_Init();	 //°´¼ü³õÊ¼»¯
+
+	while(1)     
+	{
+		a=KEY_Scan(0);
+		if(a==1)
+		{
+			LED=0;
+		}
+			
+		else if(a==2)
+		{
+			LED=1;
+		}
+	}
+}
+
